503001d80359277e8d217b942f943fa12144280d
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-03 09:28:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheese_types.include?("cheddar")
-    cheese_types[0]
+    puts cheese_types[0]
   elsif
     nil
   end
